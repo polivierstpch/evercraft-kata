d2ebacaabe48847b7352fed19a5ceff426343247
Pier-Olivier St-Pierre Chouinard <pier.olivier.stpch@gmail.com>
2020-12-09 19:23:12 -0500

    added attack method to character and added initial test

--- Evercraft.Core/Character.cs
+++ Evercraft.Core/Character.cs
@@ -4,12 +4,20 @@ namespace Evercraft.Core
 {
     public class Character
     {
+        #region Constants
         public const string DEFAULT_NAME = "No Name";
         public const Alignment DEFAULT_ALIGNMENT = Alignment.Neutral;
         public const int BASE_AC = 10;
+        public const int BASE_HITPOINTS = 5;
+        #endregion
 
         public string Name { get; set; } = DEFAULT_NAME;
         public Alignment Alignment { get; set; } = DEFAULT_ALIGNMENT;
         public int ArmorClass { get => BASE_AC; }
+        public int HitPoints { get => BASE_HITPOINTS; }
+
+        public bool Attack(int roll, Character target) => roll >= target.ArmorClass || roll == 20;
+
+
     }
 }
--- Evercraft.Tests/CharacterShould.cs
+++ Evercraft.Tests/CharacterShould.cs
@@ -6,15 +6,18 @@ namespace Evercraft.Tests
 {
     public class CharacterShould
     {
-
         private Character character = new Character();
 
         [Fact]
         public void HaveDefaultValuesAtCreation()
         {
+            // Settable properties
             character.Name.Should().Be(Character.DEFAULT_NAME);
             character.Alignment.Should().Be(Character.DEFAULT_ALIGNMENT);
+
+            // Computed properties
             character.ArmorClass.Should().Be(Character.BASE_AC);
+            character.HitPoints.Should().Be(Character.BASE_HITPOINTS);
         }
 
         [Theory]
@@ -28,6 +31,22 @@ namespace Evercraft.Tests
 
             character.Name.Should().Be(name);
             character.Alignment.Should().Be(alignment);
+        }
+
+        [Theory]
+        [InlineData(10, true)]
+        [InlineData(11, true)]
+        [InlineData(15, true)]
+        [InlineData(20, true)]
+        [InlineData(9, false)]
+        [InlineData(5, false)]
+        public void BeAbleToAttackAnotherCharacter(int roll, bool expectedResult)
+        {
+            var target = new Character();
+
+            bool attackResult = character.Attack(roll, target);
+
+            attackResult.Should().Be(expectedResult);
         }
 
     }
